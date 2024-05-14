5863d3d9f36fcce8a66deb969ed386256ebaf9a8
Jay OConnor <jdoconnor@users.noreply.github.com>
2024-05-13 17:18:18 -0700

    updates (#2)

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,19 +1,19 @@
 <script lang="ts">
-	import facebookLogo from '$lib/assets/facebook.svg?raw';
-	import githubLogo from '$lib/assets/github.svg?raw';
-	import gmailLogo from '$lib/assets/gmail.svg?raw';
-	import discordLogo from '$lib/assets/discord.svg?raw';
-	import linkedInLogo from '$lib/assets/linkedin.svg?raw';
-	import scheduleLogo from '$lib/assets/calendar.svg?raw';
-	import Icon from '$lib/icon.svelte';
+	import githubLogo from "$lib/assets/github.svg?raw";
+	import gmailLogo from "$lib/assets/gmail.svg?raw";
+	import discordLogo from "$lib/assets/discord.svg?raw";
+	import linkedInLogo from "$lib/assets/linkedin.svg?raw";
+	import scheduleLogo from "$lib/assets/calendar.svg?raw";
+	import Icon from "$lib/icon.svelte";
+	import "tailwindcss/tailwind.css";
 </script>
 
-<div id="container">
+<div class="container mx-auto">
 	<div id="content">
 		<slot />
 	</div>
-	<footer id="social">
-		<div class="icons">
+	<footer id="social" class="footer footer-center text-base-content">
+		<nav class="grid grid-flow-col gap-4">
 			<Icon
 				name="View my LinkedIn profile"
 				image={linkedInLogo}
@@ -35,52 +35,6 @@
 				image={discordLogo}
 				link="https://discordapp.com/users/257316698728693760"
 			/>
-		</div>
+		</nav>
 	</footer>
 </div>
-
-<style>
-	:global(body) {
-		background-image: url('$lib/assets/noise.png');
-		background-color: var(--bg-color);
-	}
-	/* CSS HEX */
-	:global(:root) {
-		--tiffany-blue: #84dcc6ff;
-		--columbia-blue: #d6edffff;
-		--uranian-blue: #acd7ecff;
-		--vista-blue: #8b95c9ff;
-		--viridian: #478978ff;
-
-		--bg-color: var(--columbia-blue);
-		--text-color: #282828;
-		--content-bg-color: var(--tiffany-blue);
-		--accent-color: var(--viridian);
-		--link-color: var(--uranian-blue);
-		--dark-link-color: var(--viridian);
-		--secondary-content-bg-color: #d6f1ff;
-
-		--lighten-percentage: 5%;
-		--darken-precentage: 5%;
-	}
-
-	#container {
-		width: 80%;
-		margin: 20px auto;
-	}
-
-	#content {
-		display: block;
-		border: 2px solid var(--accent-color);
-		background-color: var(--content-bg-color);
-		font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif;
-		color: var(--text-color);
-		font-size: 18px;
-		font-weight: 500;
-		border-radius: 10px;
-	}
-
-	#social {
-		text-align: center;
-	}
-</style>
