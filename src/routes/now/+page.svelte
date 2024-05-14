db3163747a563a0df44c8fc8f2c9497c3e48e2ad
Jay OConnor <jay@jayoconnor.com>
2024-05-13 17:18:03 -0700

    updates

--- src/routes/now/+page.svelte
+++ src/routes/now/+page.svelte
@@ -1,191 +1,73 @@
-<div>
-	<h1>What I'm working on and with as of Jul 11, 2023</h1>
+<script lang="ts">
+	import ProjectCard from "./ProjectCard.svelte";
+</script>
 
-	<h2>
-		Better One On Ones - <a href="https://betteroneonones.com" target="_blank"
-			>Better One on Ones.com</a
-		><span>Improving 1:1 meetings at work one at a time</span>
-	</h2>
-	<ol style="--length: 3" role="list">
-		<li style="--i: 1">
-			<h3>SvelteKit w/ Typescript</h3>
-			<p>
-				<a href="https://kit.svelte.dev/" target="_blank">SvelteKit</a> is a framework for building full
-				stack apps with Svelte. Like NuxtJS or Next.js, but for Svelte instead of React.
-			</p>
-		</li>
-		<li style="--i: 2">
-			<h3>SQLite</h3>
-			<p>
-				File based SQL database. Typescript has a lot of different options for a driver. They're all
-				fine, but slightly weird in their own way.
-			</p>
-		</li>
-		<li style="--i: 3">
-			<h3>Cloudflare</h3>
-			<p>
-				<a href="https://www.cloudflare.com">Cloudflare</a> is a platform of tools for building web and
-				mobile apps with a generous free tier. This project is using Pages (their hosting service)
-			</p>
-		</li>
-	</ol>
-
-	<h2>
-		Stuff I Use - <a href="https://stuffiuse.app/@jdoconnor" target="_blank"
-			>stuffiuse.app/@jdoconnor</a
-		><span>A "link in bio" type site for gear</span>
-	</h2>
-	<ol style="--length: 3" role="list">
-		<li style="--i: 1">
-			<h3>SvelteKit w/ Typescript</h3>
-			<p>
-				<a href="https://kit.svelte.dev/" target="_blank">SvelteKit</a> is a framework for building full
-				stack apps with Svelte. Like NuxtJS or Next.js, but for Svelte instead of React.
-			</p>
-		</li>
-		<li style="--i: 2">
-			<h3>Firebase</h3>
-			<p>
-				<a href="https://firebase.google.com/" target="_blank">Firebase</a> is a platform of tools for
-				building web and mobile apps with a generous free tier. This project uses RemoteConfig and Analytics.
-			</p>
-		</li>
-		<li style="--i: 3">
-			<h3>Cloudflare</h3>
-			<p>
-				<a href="https://www.cloudflare.com">Cloudflare</a> is a platform of tools for building web and
-				mobile apps with a generous free tier. This project is using Pages (their hosting service) and
-				D1 (sqlite database)
-			</p>
-		</li>
-	</ol>
+<div class="space-y-4">
+	<h1 class="text-3xl font-bold mb-8">What I'm working on and with as of May 13, 2024</h1>
+	<ProjectCard
+		image="/pottytracker_app.jpg"
+		description="A progeressive web app for tracking your dog's potty times with the rest of your family"
+		title="PottyTracker.app"
+		tech={[
+			{
+				name: "SvelteKit",
+				tooltip: "Full stack framework based on Svelte",
+				color: "primary-content",
+			},
+			{ name: "Tailwind", tooltip: "CSS framework", color: "primary-content" },
+			{
+				name: "Sqlite",
+				tooltip:
+					"Using Turso, a provider of sqlite that allows for edge distribution of sqlite databases",
+				color: "secondary",
+			},
+			{
+				name: "Google Cloud Run",
+				tooltip: "Serverless runtime for docker containers",
+				color: "neutral",
+			},
+			{
+				name: "Cloudflare Pages",
+				tooltip: "Serverless runtime for edge functions",
+				color: "neutral",
+			},
+		]}
+		link="https://pottytracker.app"
+	/>
+	<ProjectCard
+		title="CommonSignature.com"
+		description="An app to consolidate contract lifecycle management platforms"
+		tech={[
+			{
+				name: "SvelteKit",
+				tooltip: "Full stack framework based on Svelte",
+				color: "primary-content",
+			},
+			{ name: "Tailwind", tooltip: "CSS framework", color: "primary-content" },
+			{ name: "Hono", tooltip: "API router", color: "primary-content" },
+			{
+				name: "Google Cloud Run",
+				tooltip: "Serverless runtime for docker containers",
+				color: "neutral",
+			},
+		]}
+	/>
+	<ProjectCard
+		description="A 'link in bio' type site for gear."
+		title="stuffiuse.app"
+		tech={[
+			{ name: "Firestore", tooltip: "NOSQL Database provided by Google", color: "secondary" },
+			{
+				name: "GraphQL",
+				tooltip: "Over the wire query language for apis",
+				color: "primary-contet",
+			},
+			{
+				name: "SvelteKit",
+				tooltip: "Full stack framework based on Svelte",
+				color: "primary-content",
+			},
+		]}
+		link="https://stuffiuse.app"
+	/>
 </div>
-
-<style>
-	div {
-		padding: 20px;
-	}
-	a {
-		color: var(--dark-link-color);
-		text-decoration: none;
-	}
-
-	p {
-		margin: 0;
-	}
-
-	ol {
-		list-style: none;
-		counter-reset: list;
-		padding: 0 1rem;
-	}
-
-	li {
-		--stop: calc(100%);
-		--l: 62%;
-		--l2: 88%;
-		--h: calc((var(--i) - 1) * (180 / var(--length)));
-		--c1: hsl(var(--h), 71%, var(--l));
-		--c2: hsl(var(--h), 71%, var(--l2));
-
-		position: relative;
-		counter-increment: list;
-		max-width: 45rem;
-		margin: 2rem auto;
-		padding: 2rem 1rem 1rem;
-		box-shadow: 0.1rem 0.1rem 1.5rem rgba(0, 0, 0, 0.3);
-		border-radius: 0.25rem;
-		overflow: hidden;
-		background-color: var(--secondary-content-bg-color);
-	}
-
-	li::before {
-		content: '';
-		display: block;
-		width: 100%;
-		height: 1rem;
-		position: absolute;
-		top: 0;
-		left: 0;
-		background: var(--accent-color);
-	}
-
-	h3 {
-		display: flex;
-		align-items: baseline;
-		margin: 0 0 1rem;
-		color: var(--text-color);
-		position: relative;
-		padding: 0;
-		margin: 0;
-		font-family: sans-serif;
-		font-weight: 300;
-		font-size: 40px;
-		-webkit-transition: all 0.4s ease 0s;
-		-o-transition: all 0.4s ease 0s;
-		transition: all 0.4s ease 0s;
-	}
-
-	h3::before {
-		display: flex;
-		justify-content: center;
-		align-items: center;
-		flex: 0 0 auto;
-		margin-right: 1rem;
-		width: 3rem;
-		height: 3rem;
-		padding: 1rem;
-		border-radius: 50%;
-		background-color: var(--dark-link-color);
-	}
-
-	@media (min-width: 40em) {
-		li {
-			margin: 3rem auto;
-			padding: 3rem 2rem 2rem;
-		}
-
-		h3 {
-			font-size: 2.25rem;
-			margin: 0 0 2rem;
-		}
-
-		h3::before {
-			margin-right: 1.5rem;
-		}
-	}
-
-	h2 span {
-		font-size: 13px;
-		font-weight: 500;
-		text-transform: uppercase;
-		letter-spacing: 4px;
-		padding-left: 0.25em;
-		color: rgba(0, 0, 0, 0.4);
-		padding-bottom: 10px;
-		display: block;
-		font-size: 0.5em;
-	}
-	h2 {
-		position: relative;
-		padding: 0;
-		margin: 1em;
-		font-weight: 300;
-		font-size: 40px;
-	}
-
-	h1 {
-		font-weight: 400;
-		font-size: 40px;
-	}
-
-	h1,
-	h2,
-	h3 {
-		font-family: sans-serif;
-		color: var(--text-color);
-		-webkit-transition: all 0.4s ease 0s;
-		-o-transition: all 0.4s ease 0s;
-		transition: all 0.4s ease 0s;
-	}
-</style>
---
+++ src/routes/now/ProjectCard.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+	type Props = {
+		image?: string;
+		description: string;
+		title: string;
+		tech: { name: string; tooltip: string; color?: string }[];
+		link?: string;
+	};
+	let { image, description, title, tech, link }: Props = $props();
+	// badge-primary badge-primary-content badge-secondary-content badge-secondary badge-neutral badge-ghost badge-accent badge-info badge-success badge-warning badge-error
+</script>
+
+<div class="card lg:card-side bg-base-100 shadow-xl margin-md">
+	<figure>
+		{#if image}
+			<img src={image} alt={title} class="max-h-96" />
+		{/if}
+	</figure>
+	<div class="card-body">
+		<h2 class="card-title">{title}</h2>
+		<p>{description}</p>
+		<div class="badges">
+			{#each tech as { name, tooltip, color }}
+				<div class="tooltip" data-tip={tooltip}>
+					<div class="badge badge-{color ?? 'primary'}">{name}</div>
+				</div>
+			{/each}
+		</div>
+		<div class="card-actions justify-end">
+			{#if link}
+				<a href={link} target="_blank btn btn-primary">Go to app</a>
+			{/if}
+		</div>
+	</div>
+</div>
